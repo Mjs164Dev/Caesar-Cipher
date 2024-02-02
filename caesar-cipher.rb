41ef3d0a8b61b86e7b809acfb4d32512c0c1f1d9
Mike Schmid <152652501+Mjs164Dev@users.noreply.github.com>
2024-02-02 10:52:45 -0500

    start mapping through message in caeser cipher method

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,8 +1,10 @@
+# ask user to input message
 puts "Please enter your message:"
-message = gets.chomp
+message = gets.chomp # gets.chomp returns a string. That's what we want here.
 
-shift_factor = nil
+shift_factor = nil # define the shift_factor
 
+# loop until the shift factor is verified as an integer
 until shift_factor.is_a?(Integer)
   puts "Please enter a shift factor:"
   input = gets.chomp
@@ -15,9 +17,17 @@ until shift_factor.is_a?(Integer)
   end
 end
 
+# ceaser cipher method that accepts message and shift factor as parameters and returns the encrypted string.
 def caesar_cipher(message, shift_factor)
-  sum = 5 + shift_factor
-  puts sum
+  alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+  message_array = message.rstrip.chars # delete spaces off end of string and split string into an array of chars
+  message_array.map.with_index do |char, index|
+    
+    # match index of char in alphabet
+    # new_char = alphabet[]
+
+    puts "#{char} & #{new_char}"
+  end
 end
 
 caesar_cipher(message, shift_factor)
