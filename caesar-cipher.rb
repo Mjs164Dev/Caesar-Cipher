1fa78c90840f34c5e29ee68d39a0ad459b71a4f2
Mike Schmid <152652501+Mjs164Dev@users.noreply.github.com>
2024-02-08 13:38:56 -0500

    Remove debugging puts messages and fix encrypted string . join spacing.

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -19,7 +19,6 @@ end
 
 # ceaser cipher method that accepts message and shift factor as parameters and returns the encrypted string.
 def caesar_cipher(message, shift_factor)
-  
   # define local variables
   new_string = []
   wrap_amaount = 0
@@ -28,40 +27,33 @@ def caesar_cipher(message, shift_factor)
 
   # delete spaces off end of string and split string into an array of chars
   message_array = message.rstrip.chars
-  puts "message_array = #{message_array}"
 
   # Shift each character in message
   message_array.map do |char|
     ascii_value = char.ord
     shifted_ascii_value = ascii_value - shift_factor
     wrap_amaount = 0
-
     if ascii_value.between?(97,122) #check if char is lower case
       if shifted_ascii_value < 97
         wrap_amaount = 97 - shifted_ascii_value
         new_char = (122 - wrap_amaount).chr
-        puts ("new char #{char} was wrapped! It's now #{new_char}!")
       else
         new_char = shifted_ascii_value.chr
-        puts ("new char #{char} wasn't wrapped! It's now #{new_char}!")
       end
     elsif ascii_value.between?(65,90) #check if char is upper case
       if shifted_ascii_value < 65
         wrap_amaount = 65 - shifted_ascii_value
         new_char = (90 - wrap_amaount).chr
-        puts ("new char #{char} was wrapped! It's now #{new_char}!")
       else
         new_char = shifted_ascii_value.chr
-        puts ("new char #{char} wasn't wrapped! It's now #{new_char}!")
       end
     else # if char isn't upper or lower case, then don't do anything to it
       new_char = char 
-      puts ("special char #{char} was pushed to new_string!")
     end
     new_string.push(new_char)
   end
-  new_string = new_string.join(" ")
-  puts "#{new_string}"
+  new_string = new_string.join("")
+  puts "Encrypted message: #{new_string}"
 end
 
 caesar_cipher(message, shift_factor)
